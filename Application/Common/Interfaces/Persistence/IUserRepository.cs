22329942ba4e9719f1c872ffbea1d9d78faa470e
M Hassan Rafay <63226790+hasan-rafay-tplex@users.noreply.github.com>
2024-01-29 20:12:05 +0100

    Added user model and repository

---
+++ Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces.Persistence
+{
+    public interface IUserRepository
+    {
+        User GetUserByIdAsync(Guid id);
+
+        void AddAsync(User user);
+    }
+}
---
+++ Infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Interfaces.Persistence;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class UserRepository : IUserRepository
+    {
+        private static readonly List<User> _users = new();
+
+        public void AddAsync(User user)
+        {
+            _users.Add(user);
+        }
+
+        public User? GetUserByIdAsync(Guid id)
+        {
+           return _users.Where(x=>x.Id == id).FirstOrDefault();
+        }
+    }
+}
