215203315a2c9de5098c190e6429972c3adcbb22
M Hassan Rafay <63226790+hasan-rafay-tplex@users.noreply.github.com>
2024-02-07 20:01:07 +0100

    Added a middleware in a request

--- HRBBoilerplate/Program.cs
+++ HRBBoilerplate/Program.cs
@@ -1,4 +1,5 @@
 using HRBBoilerplate.Filters;
+using HRBBoilerplate.Middleware;
 using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CustomMiddleware>();
 
 app.UseHttpsRedirection();
 
